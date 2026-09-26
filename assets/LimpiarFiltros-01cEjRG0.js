d891b7f3ed5e08973a5baf29e2029d88e8ebe4d1
hjsanoja <130082161+hjsanoja@users.noreply.github.com>
2026-09-26 05:21:45 +0000

    Deploying to gh-pages from @ hjsanoja/TrackFlow-MD3-v4@304823fd2a6eae0b14d18f65bc76064070e6a84c 🚀

---
+++ assets/LimpiarFiltros-01cEjRG0.js
@@ -0,0 +1,1 @@
+import{j as i}from"./index-GOH6CRfC.js";function a({visible:t,onClick:r}){return i.jsx("button",{type:"button",onClick:r,className:`m3-btn-text ${t?"":"invisible"}`,"aria-hidden":!t,tabIndex:t?0:-1,children:"Limpiar filtros"})}export{a as L};
